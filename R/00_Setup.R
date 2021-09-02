193352ca245a44d5472246c37f8a47ff349f0e9b
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-09-02 15:22:59 -0400

    init

---
+++ R/00_Setup.R
@@ -0,0 +1,47 @@
+
+# Load libraries required for analyses.
+library(isocat)
+library(dplyr)
+library(sf)
+
+# Load libraries relied on for a few analyses.
+library(assignR)
+library(chron)
+library(geosphere)
+library(ggmap)
+library(ggplot2)
+library(lubridate)
+library(lwgeom)
+library(measurements)
+library(purrr)
+library(readxl)
+library(rgdal)
+library(rmapshaper)
+library(smatr)
+library(stringr)
+library(tidyr)
+
+# Make an object to help navigate the subdirectories.
+# Be sure to fill this in if you're reproducing these analyses!
+my_dir_path <- "/Users/cjcampbell/WISN_dD"
+if(!exists("wd")) { wd <- list() }
+wd$R       <- file.path( my_dir_path, "R" )
+wd$bin     <- file.path( my_dir_path, "bin" )
+wd$data    <- file.path( my_dir_path, "data" )
+wd$figs    <- file.path( my_dir_path, "figs" )
+
+# Check for presence of subdirectories. Create if needed.
+invisible({
+  lapply(wd, function(i) if( dir.exists(i) != 1 ) dir.create(i) )
+})
+
+
+# Define extent of spatial analysis.
+# Units == meters
+my_extent_aea <- raster::extent(
+  -16e5, 34e5,
+  -30e5, 16e5
+)
+
+# CRS for aea projection:
+myCRS <- "+proj=aea +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs +towgs84=0,0,0"
---
+++ R/02_LoadSpatialData.R
@@ -0,0 +1,154 @@
+
+# Setup -------------------------------------------------------------------
+download_GADM <- FALSE
+
+# This script assumes that there are candidate isoscapes downloaded from isoMAP
+# somewhere in the wd$data directory.
+reload_isoscapes <- FALSE
+
+# This script assumes that there are IUCN rangemaps somewhere in the wd$iucn
+# directory.
+reload_IUCN_rangemaps <- FALSE
+
+# Load GADM data ----------------------------------------------------------
+if(download_GADM == TRUE){
+
+  library(rmapshaper)
+  message("Loading GADM data...")
+
+  # First, make NoAm_sf (an "sf" object) fit extent object (my_extent).
+  # my_extent_aea_st <- st_bbox(st_transform(st_as_sfc(st_bbox(my_extent, crs = 4326)), myCRS))
+  # saveRDS(my_extent_aea_st, file = file.path(wd$bin, "my_extent_aea_st.rds"))
+
+  # Get GADM data to state level.
+  USA <- raster::getData('GADM', path = wd$bin, country='USA', level=0)
+  MEX <- raster::getData('GADM', path = wd$bin, country='MEX', level=0)
+  CAN <- raster::getData('GADM', path = wd$bin, country='CAN', level=0)
+  GTM <- raster::getData('GADM', path = wd$bin, country='GTM', level=0)
+
+  # Prepare to remove areas outside of desired extent.
+  # ## Remove Hawaii
+  USA_1 <- raster::getData('GADM', path = wd$bin, country='USA', level=1)
+  hawaii <- USA_1[USA_1@data$NAME_1 == "Hawaii",] # Select Hawaii
+  hawaii_simpl <- hawaii %>%
+    sf::st_as_sf() %>%
+    st_transform(crs = myCRS) %>%
+    st_simplify(preserveTopology = FALSE, dTolerance = 5000) %>%
+    st_buffer(dist = 1e6)
+  # ## Remove water bodies.
+  USA_2 <- raster::getData('GADM', path = wd$bin, country='USA', level=2)
+  MEX_2 <- raster::getData('GADM', path = wd$bin, country='MEX', level=2)
+  CAN_2 <- raster::getData('GADM', path = wd$bin, country='CAN', level=2)
+  GTM_2 <- raster::getData('GADM', path = wd$bin, country='GTM', level=2)
+  waterbodies <- lapply(list(USA_2,MEX_2,CAN_2,GTM_2), function(x){
+    x[(x$ENGTYPE_2) == "Water body",]
+  }) %>%
+    do.call(rbind, .) %>%
+    sf::st_as_sf() %>%
+    st_transform(crs = myCRS) %>%
+    st_simplify(preserveTopology = FALSE, dTolerance = 5000)
+
+  # Combine into one polygon, convert to sf object.
+  NoAm <- raster::bind(
+    MEX, USA, CAN, GTM#, BLZ, SLV, HND, NIC
+  ) %>%
+    sf::st_as_sf(.) %>%
+    st_transform(crs = myCRS) %>%
+    st_simplify(preserveTopology = FALSE, dTolerance = 5e3) %>%
+    st_difference(., hawaii_simpl) # Remove Hawaii
+
+  saveRDS(NoAm, file = file.path(wd$bin, "NoAm.rds"))
+
+  NoAm_boundary_aea <- NoAm %>%
+    st_buffer(dist = 5e4) %>%
+    rmapshaper::ms_erase(., waterbodies)  # Remove water bodies
+
+  saveRDS(NoAm_boundary_aea, file = file.path(wd$bin, "NoAm_boundary_aea.rds"))
+
+} else message("Not redownloading GADM Data...")
+
+
+# Load isoscapes ----------------------------------------------------------
+
+if(reload_isoscapes == TRUE){
+  message("reloading isoscapes...")
+
+  NoAm_boundary_aea <- readRDS( file.path(wd$bin, "NoAm_boundary_aea.rds") )
+
+  # Function to extend/crop/mask by above.
+  ECM <- function(rasterLayer){
+    rasterLayer %>%
+      raster::projectRaster(., crs = myCRS) %>%
+      raster::extend( ., my_extent_aea ) %>%
+      raster::crop(   ., my_extent_aea ) %>%
+      # Following line not needed-- no need to resample for one surface.
+      #raster::resample(., refIsoscape) %>%
+      raster::mask(   ., NoAm_boundary_aea  )
+  }
+
+  myisoscape <- list()
+  myisoscape$directory          <- file.path(wd$data, "isoscape")
+  myisoscape$path_pattern       <- "66100"
+  myisoscape$isoscape_pattern   <- "predkrig.tiff$"
+  myisoscape$sd_pattern         <- "stdkrig.tiff$"
+
+  myisoscape$isoscape <- list.files(
+    myisoscape$directory, pattern =  myisoscape$isoscape_pattern,
+    recursive = TRUE, full.names = TRUE
+  ) %>%
+    grep(myisoscape$path_pattern, ., value = TRUE) %>%
+    raster::raster(.) %>%
+    ECM(.)
+
+  myisoscape$sd <- list.files(
+    myisoscape$directory, pattern = myisoscape$sd_pattern,
+    recursive = TRUE, full.names = TRUE
+  ) %>%
+    grep(myisoscape$path_pattern, ., value = TRUE) %>%
+    raster::raster(.) %>%
+    ECM(.)
+
+  # Save.
+  save(myisoscape, file = file.path(wd$bin, "my_isoscapes.RData"))
+
+} else message("Not reloading isoscapes, loading saved version...")
+
+
+# Load and buffer IUCN Rangemaps -----------------------------------------------------
+if(reload_IUCN_rangemaps == TRUE){
+
+  load(file.path(wd$bin, "my_isoscapes.RData"), verbose = TRUE)
+  if(!exists("NoAm_boundary_aea")) {
+    NoAm_boundary_aea <- readRDS( file.path(wd$bin, "NoAm_boundary_aea.rds") )
+  }
+
+  locationIUCNData <- dir(wd$data, pattern = "redlist_species_data", full.names = T)
+
+  # Load IUCN rangemap.
+  IUCNmaps <-
+    locationIUCNData %>%
+    st_read(layer = "data_0")
+
+  # Convert to simple features object and reproject to myCRS.
+  range_tidy <- IUCNmaps %>%
+    st_as_sf(crs = 4326) %>%
+    st_transform(crs = myCRS) %>%
+    st_simplify(preserveTopology = TRUE, dTolerance = 5000) %>%
+    st_make_valid() %>%
+    st_buffer(dist = 10e3) %>%
+    st_combine()
+
+  # Convert buffered rangemaps to rasters with appropriate
+  ex_rast <- myisoscape$isoscape
+  ex_rast[] <- 1
+  range_raster <- raster::mask(
+    ex_rast,
+    mask = as_Spatial(range_tidy),
+    updatevalue = NA
+    ) %>%
+    raster::mask(., NoAm_boundary_aea) %>%
+    raster::crop(., my_extent_aea)
+
+  saveRDS(range_raster, file = file.path(wd$bin, "range_raster.rds" ))
+
+} else message("Not reloading IUCN rangemaps...")
