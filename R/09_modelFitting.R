67606b47adfd39dc225a3f429645f4a8fd3ec62c
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2022-12-10 15:59:59 -0500

    add plots

--- R/09_modelFitting.R
+++ R/09_modelFitting.R
@@ -1,6 +1,8 @@
+library(data.table)
+library(sf)
+library(ggstatsplot)
 
 # Exploratory direction analyses ------------------------------------------
-
 
 allResults <- fread(file.path(wd$bin, "allResults.csv")) %>%
   dplyr::mutate(
@@ -102,3 +104,141 @@ allResults %>%
   ) +
   theme_minimal()
 ggsave(file.path(wd$figs, "lat-v-MinDist.png"))
+
+
+
+# boxplots ----------------------------------------------------------------
+
+mydata_transformed <- readRDS( file.path(wd$bin, "mydata_transformed.rds") )
+load(file.path(wd$bin, "mydata_clustered.Rdata"))
+df <- left_join(mydata_transformed, mydata_clustered) %>%
+  left_join(allResults) %>%
+  dplyr::filter(!is.na(d2H)) %>%
+  dplyr::mutate(
+    year = lubridate::year(Date),
+    winter = case_when(Date <= as.Date("2021-06-15") ~ "2020-1", Date > as.Date("2021-06-15") ~ "2021-2")
+    )
+
+ggstatsplot::ggbetweenstats(
+  data = df, y = d2H, x = winter,
+  ggplot.component	= list(
+    ggplot2::theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    ),
+    ggplot2::ylab(expression(bold(paste(delta^2 ~ H[feather], " (", "\u2030", ", VSMOW)") ))),
+    ggplot2::xlab("Collection year")
+    )
+)
+
+ggstatsplot::ggbetweenstats(
+  data = df, y = d2H, x = OriginCluster,
+  ggplot.component	= list(
+    ggplot2::theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    ),
+    ggplot2::ylab(expression(bold(paste(delta^2 ~ H[feather], " (", "\u2030", ", VSMOW)") )))
+  )
+  )
+
+# ggplot(df) +
+#   geom_boxplot(aes(y=d2H, x = OriginCluster, group = OriginCluster))
+
+
+# Dist by cluster ---------------------------------------------------------
+
+set.seed(42); allResults %>%
+  dplyr::mutate(
+    westOrigin = case_when(
+    probWestOrigin >= 0.8 ~ "W",
+    probWestOrigin <= 0.2 ~ "E",
+    TRUE ~ "Unknown"
+  )) %>%
+  ggplot() +
+  geom_point(aes(y = dist_km, color = westOrigin, x = OriginCluster), position = position_jitter(width = 0.33)) +
+  scale_y_continuous("Minimum distance traveled (km)", breaks = seq(0,4000,by = 500), limits = c(0,5000)) +
+  scale_color_manual(
+    "Region of origin",
+    values = c("#D11149", "#AE7709", "black"),
+    breaks = c("E","W", "Unknown"),
+    labels = c("East (> -100°W)", "West (<= -100°W)", "Region unspecified"),
+    na.value = "#29335C"
+  ) +
+  theme_minimal() +
+  theme(
+    legend.position = c(0.1,0.8),
+    axis.ticks = element_line(),
+    panel.grid = element_blank()
+  )
+
+
+
+
+ggstatsplot::ggbetweenstats(
+  data = allResults, y = dist_km, x = OriginCluster,
+  ggplot.component	= list(
+    ggplot2::theme(
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank()
+    ),
+    ggplot2::ylab(expression(bold(paste(delta^2 ~ H[feather], " (", "\u2030", ", VSMOW)") )))
+  )
+)
+
+allResults %>%
+  ggplot() +
+  geom_histogram(aes(dist_km, group = factor(OriginCluster), fill = factor(OriginCluster)), binwidth = 250) +
+  scale_x_continuous("Minimum distance traveled (km)", breaks = seq(0,4000,by = 500), limits = c(0,5000)) +
+  scale_y_continuous(expand = c(0,1)) +
+  scale_fill_viridis_d("OriginCluster") +
+  theme_minimal() +
+  theme(
+    legend.position = c(0.1,0.8),
+    axis.ticks = element_line(),
+    panel.grid = element_blank()
+  )
+
+
+
+
+# distance over time --------------------------------------------------------------------
+
+allResults %>%
+  ggplot() +
+  geom_point(aes(x=Date, y = dist_km))
+
+  geom_histogram(aes(dist_km, group = factor(OriginCluster), fill = factor(OriginCluster)), binwidth = 250) +
+  scale_x_continuous("Minimum distance traveled (km)", breaks = seq(0,4000,by = 500), limits = c(0,5000)) +
+  scale_y_continuous(expand = c(0,1)) +
+  scale_fill_viridis_d("OriginCluster") +
+  theme_minimal() +
+  theme(
+    legend.position = c(0.1,0.8),
+    axis.ticks = element_line(),
+    panel.grid = element_blank()
+  )
+
+
+# Clusters by year --------------------------------------------------------
+
+allResults %>%
+    dplyr::mutate(
+      year = lubridate::year(Date),
+      winter = case_when(Date <= as.Date("2021-06-15") ~ "2020-1", Date > as.Date("2021-06-15") ~ "2021-2")
+    ) %>%
+    ggplot() +
+    geom_bar(aes( fill= OriginCluster,x= winter, group = OriginCluster), position = "dodge")
+
+allResults %>%
+  dplyr::mutate(
+    year = lubridate::year(Date),
+    winter = case_when(Date <= as.Date("2021-06-15") ~ "2020-1", Date > as.Date("2021-06-15") ~ "2021-2")
+  ) %>%
+  dplyr::group_by(winter, OriginCluster) %>%
+  dplyr::summarise(n=n()) %>%
+  pivot_wider(names_from = winter, values_from = n) %>%
+  dplyr::select(-OriginCluster) %>%
+  chisq.test()
+
+
